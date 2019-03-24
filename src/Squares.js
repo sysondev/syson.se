385358b071c9d22a6914d349d49911fa3cccef65
Love Gehlin <love.gehlin@gmail.com>
2019-03-24 13:55:38 +0100

    add link to gasell article

--- src/Squares.js
+++ src/Squares.js
@@ -1,39 +1,55 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 import styles from './Squares.module.css';
 import arrow from './arrow.svg';
-import Emoji from './Emoji'
+import Emoji from './Emoji';
 import pencil from './emojis/pencil.png';
-import woman from './emojis/woman_office_worker.png'
-import trophy from './emojis/trophy.png'
-
-
+import woman from './emojis/woman_office_worker.png';
+import trophy from './emojis/trophy.png';
 
 const Square = ({ emoji, title, description, url, color }) => (
   <div className={classNames(styles.square, styles[color])}>
     <div className={styles.content}>
       <div>
-        <p className={styles.emoji}><Emoji src={emoji} /></p>
+        <p className={styles.emoji}>
+          <Emoji src={emoji} />
+        </p>
         <h3>{title}</h3>
       </div>
-      <a
-        href={url}
-        className={styles.link}
-      >
+      <Link to={url} className={styles.link}>
         <span className={styles.linkText}>{description}</span>{' '}
         <img src={arrow} />
-      </a>
+      </Link>
     </div>
-  </div >
-)
+  </div>
+);
 
 export default () => {
   return (
     <section>
       <div className={classNames('container', styles.squares)}>
-        <Square emoji={pencil} title="Hur vi har räknat ut lönen" description="Läs mer" color="tradewind" url="/" />
-        <Square emoji={woman} title="Hur är konsultlivet?" description="Läs mer" color="cashmere" url="/" />
-        <Square emoji={trophy} title="Man får skryta ibland" description="Läs mer" color="citrus" url="/" />
+        <Square
+          emoji={pencil}
+          title="Hur vi har räknat ut lönen"
+          description="Läs mer"
+          color="tradewind"
+          url="/"
+        />
+        <Square
+          emoji={woman}
+          title="Hur är konsultlivet?"
+          description="Läs mer"
+          color="cashmere"
+          url="/"
+        />
+        <Square
+          emoji={trophy}
+          title="Man får skryta ibland"
+          description="Läs mer"
+          color="citrus"
+          url="/gasell"
+        />
       </div>
     </section>
   );
