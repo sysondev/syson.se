12ce72f32d8291ac8c7560b077b44cfe572f4b68
Soroush Hakami <soroush.hakami@gmail.com>
2019-03-24 09:31:17 +0100

    correct people cv-links

--- src/People.js
+++ src/People.js
@@ -32,81 +32,81 @@ const isTouchDevice = () => {
 const people = shuffle([
   {
     name: 'Alexander Hedberg',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/e/2PACX-1vQWXShOcs8-aZ3i516973hCPjy_N_jvfk0BM5M6LCR3jBbgviCVm9IPpGevjt8ywkzfTIakMnYGRYoT/pub',
     image: '/images/people/alexh.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Johanna Lagerholm',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1FTaXx83ry7rS5vMPMyywwaQ_XqRk-3VBmQV5Pi-7-yc/pub',
     image: '/images/people/johanna.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Leo Danielsson',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/e/2PACX-1vSxiEc0TSynbc_BFIo2Yr8f-oBzkKkHtYuVcDsNclXb1N3LO0Y5UAahoGoJ2gzJUAF4CIV9YFzJ4RHK/pub',
     image: '/images/people/leo.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Alexander Lissenko',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/e/2PACX-1vSFPFoMEGbFYSKwNHw1fjbEsEydunSKPyG2BPoIyoaj-4wQ1ARCWV9YnqoRL-_DZu6bkuT3ova-TkCY/pub',
     image: '/images/people/alexander.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Olov Gullikson',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/e/2PACX-1vQR6JrgxZw2vG9Sc0C9XeuDs59uhXo-7edCIN_7PpYDPCJNDUid9a3McxW4OB2TNlWKuJcfBSceRC5I/pub',
     image: '/images/people/olov.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Anton Söderstedt',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1vZeB8Jxe7qM4qVV8wJBM6X2zGvQKCUEhkVY4-vTFGtk/pub',
     image: '/images/people/anton.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Mikael Larsson',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/13QeSY4v8b5hOBbZPC3SkdaZqivr-D3sGh4jEj-EUCag/pub',
     image: '/images/people/micke.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Otto Nordgren',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1NwXJjs6DdApM_Qg04mQY8raWkY7IrPeaUxIa64TkIDM/pub',
     image: '/images/people/otto.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Magnus Linell',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1aTuje5x5GmgnupA6zcyPqvzm-mRebmGIO_z0JTn_4ao/pub',
     image: '/images/people/magnus.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Katrine Johansson',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1TZj2yAhj6acUkCCTEomhN2GXQNFQHp2HRt6ksnaJP2M/pub',
     image: '/images/people/katrine.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'David Skog',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1ykZhFpRhlcc2CH5JnDdna6fZId4l4stmS78A92BqXh0/pub',
     image: '/images/people/david.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Joakim Gordillo',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/a/syson.se/document/d/1zNxgPpG5LfE35n1OLF3rB6iKIHbZBUCbo__h9wOC2wE/pub',
     image: '/images/people/joakim.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Patrick Thomsson',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/1GuaMyViqwnT8Nysba9n3LKW2CBJesuORIiGlY93twqk/pub',
     image: '/images/people/patrick.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Soroush Hakami',
@@ -116,15 +116,15 @@ const people = shuffle([
   },
   {
     name: 'Love Gehlin',
-    profileUrl: '//google.se',
+    profileUrl: 'https://docs.google.com/document/d/13WyjMVR69WEQ75vV5UPBtdOjLMaqUFTyZLAwPr90Hvk/pub',
     image: '/images/people/love.jpg',
-    quote: 'Jag jobbar med data'
+    quote: ''
   },
   {
     name: 'Anders Söderstedt',
     image: '/images/people/anders.jpg',
     linkedInUrl: 'https://www.linkedin.com/in/anders-s%C3%B6derstedt-1631b8/',
-    quote: 'Jag jobbar med data'
+    quote: ''
   }
 ]);
 
