6ad6976b5a13b7ee2a3e366df2c0b93fa2095cfd
Soroush Hakami <soroush.hakami@gmail.com>
2019-03-29 14:56:31 +0100

    improved contact-section

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Contact.module.css';
-import starbox from './starbox.png';
+import sysonman from './sysonman.png';
 export default () => {
   return (
     <section>
@@ -8,19 +8,21 @@ export default () => {
         <h2>Säg hej!</h2>
         <div className={styles.contact}>
           <div className={styles.left}>
-            <p>Vi sitter på The Parks lokaler i City, Vasastan & Södermalm.</p>
-            <a href='mailto:hej@syson.se' className={styles.mail}>
-              hej@syson.se
-            </a>
+            <p>Vi vill alltid höra från trevligt folk. </p>
+            <p>Fikasugen? Vi sitter på The Parks lokaler i City, Vasastan & Södermalm.</p>
           </div>
           <div className={styles.list}>
             <div>Soroush</div>
             <a href='tel:0730473441'>073-047 34 41</a>
             <div>Anders</div>
             <a href='tel:0733347999'>073-334 79 99</a>
+            <div></div>
+            <a href='mailto:hej@syson.se'>
+              hej@syson.se
+            </a>
           </div>
-          <div>
-            <img src={starbox} />
+          <div className={styles.sysonmanContainer}>
+            <img className={styles.sysonman} src={sysonman} />
           </div>
         </div>
       </div>
