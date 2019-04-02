01ea6e9a243c2c700013c89a08b0676f5b4ae181
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-02 10:39:16 +0200

    gasell-page (still hidden)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Footer from './Footer';
-import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import ArticlePage from './ArticlePage';
--- src/GasellArticle.js
+++ src/GasellArticle.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import styles from './GasellArticle.module.css';
+import gasell from './gasell.png';
 
 export default () => {
   return <>
+
     <h1>Ibland är det OK att skryta</h1>
+
+    <div className={styles.imageContainer}><img className={styles.image} src={gasell} /></div>
 
     <p>Att få brev i postlådan brukar inte vara så kul nu för tiden. Det är mest reklam och räkningar. Men nyligen fick vi ett brev från Dagens Industri som vi blev glada för.</p>
 
-    <quote>"Syson uppfyller alla högt ställda krav på ett Gasellföretag!
+    <b>"Syson uppfyller alla högt ställda krav på ett Gasellföretag!
     Vi på Dagens Industri tycker att det gör er till några av Sveriges hjältar.
     Ni har byggt ett av Sveriges snabbast växande och mest lönsamma företag.
     Ni skapar jobb för människor, ni driver tillväxt för Sverige och inspirerar andra att följa i era fotspår”
-    </quote>
+    </b>
 
     <p>Vi tackar, vi tackar!</p>
 
-    <p>DI Gasell är ett pris som delas ut till företag som har visat en stark tillväxt på ett ekonomiskt hälsosamt sätt.</p>
+    <i>DI Gasell är ett pris som delas ut till företag som har visat en stark tillväxt på ett ekonomiskt hälsosamt sätt.</i>
   </>;
 };
