7d996d68d3df8b5aa7cfebe0250b0fbdb53690c1
Leo Danielsson <leo.danielsson@comhem.com>
2019-04-08 20:54:27 +0200

    optimize hero image

--- src/Header.js
+++ src/Header.js
@@ -4,13 +4,27 @@ import styles from './Header.module.css';
 import logo from './logo.png';
 import Emoji from './Emoji'
 import wavingHand from './emojis/waving-hand.png'
+import ottoSmallWebp from './otto-800.webp';
+import ottoBigWebp from './otto-1920.webp';
+import ottoSmall from './otto-800.jpg';
+import ottoBig from './otto-1920.jpg';
+import ottoFallback from './otto.jpg';
 
 export default () => (
     <header className={styles.header}>
-      <Link to="/"><img src={logo} alt="Syson Logo" className={styles.logo} /></Link>
-      <div className={styles.headline}>
-        <h1>Frihet, bra häng och schysst lön</h1>
-        <p>Hej! <Emoji src={wavingHand} /> Vi är konsulter i Stockholm, duktiga inom systemutveckling & test.</p>
+      <picture className={styles.heroImage} >
+        <source media="(min-width: 450px)" srcSet={ottoBigWebp} type="image/webp" />
+        <source media="(min-width: 450px)" srcSet={ottoBig} type="image/jpeg" /> 
+        <source srcSet={ottoSmallWebp} type="image/webp" />
+        <source srcSet={ottoSmall} type="image/jpeg" />
+        <img src={ottoFallback} alt="" />
+      </picture>
+      <div className={styles.content}>
+        <Link to="/"><img src={logo} alt="Syson Logo" className={styles.logo} /></Link>
+        <div className={styles.headline}>
+          <h1>Frihet, bra häng och schysst lön</h1>
+          <p>Hej! <Emoji src={wavingHand} /> Vi är konsulter i Stockholm, duktiga inom systemutveckling & test.</p>
+        </div>
       </div>
     </header>
   );
