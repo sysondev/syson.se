a28d20ff46d6b58b3d09b905b58afe34831f8954
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-12 11:09:37 +0200

    kalkylen looks good on desktop (not yet good on mobile)

--- src/Kalkylen.js
+++ src/Kalkylen.js
@@ -12,41 +12,37 @@ export default () => {
             <input className={styles.input} />
           </div>
           <div>
-            <label className={styles.label}>Timpris</label>
+            <label className={styles.label}>Procent av heltid</label>
             <input className={styles.input} />
           </div>
           <div>
-            <label className={styles.label}>Timpris</label>
+            <label className={styles.label}>Sjukdagar / Övrig frånvaro</label>
             <input className={styles.input} />
           </div>
           <div>
-            <label className={styles.label}>Timpris</label>
+            <label className={styles.label}>Övrigt</label>
             <input className={styles.input} />
+            <label className={classNames(styles.label, styles.labelSmall)}>Telefon, tjänstebil, mm</label>
           </div>
           <div>
-            <label className={styles.label}>Timpris</label>
+            <label className={styles.label}>Extra pensionsavsättning</label>
             <input className={styles.input} />
+            <label className={classNames(styles.label, styles.labelSmall)}>Utöver 2600 kr</label>
           </div>
         </div>
       </div>
       <div className={styles.results}>
         <div className={classNames(styles.result, styles.first)}>
-          <span className={styles.resultLabel}>2018 antal dagar kvar</span>
-          <span className={styles.resultValue}>250</span>
+          <span className={styles.resultLabel}>2019 antal dagar</span>
+          <span className={styles.resultValue}>253</span>
         </div>
         <div className={classNames(styles.result)}>
           <span className={styles.resultLabel}>Total månadspension</span>
-          <span className={styles.resultValue}>2500</span>
+          <span className={styles.resultValue}>2600</span>
         </div>
         <div className={classNames(styles.result, styles.highlight)}>
           <span className={styles.resultLabel}>Månadslön</span>
           <span className={styles.resultValue}>5 miljoner!</span>
-        </div>
-        <div className={classNames(styles.result)}>
-          <span className={styles.resultLabel}>
-            Lön med rekommenderad reserv
-          </span>
-          <span className={styles.resultValue}>4 miljoner</span>
         </div>
       </div>
     </div>
