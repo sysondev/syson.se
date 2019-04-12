d4c6fc3a11e249f50af3d08fa72511851fbdc4f8
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-12 14:13:42 +0200

    first working prototype

--- src/Kalkylen.js
+++ src/Kalkylen.js
@@ -7,17 +7,34 @@ class Kalkylen extends React.Component {
     super(props);
     this.state = {
       hourlyRate: 900,
-      employmentPercentage: 100,
       sickDays: 10,
-      monthlyCosts: 1000,
+      extraVacation: 0,
+      otherMonthlyCosts: 1000,
       extraPension: 0,
+      numberOfDays: 253,
+      defaultPension: 2600,
     };
+  }
+
+  setSalary = () => {
+    const salary =
+      Math.round(((((this.state.numberOfDays / 12) - ((this.state.sickDays + this.state.extraVacation) / 12) - 2.5) * (this.state.hourlyRate * 8 * 0.7))
+        - ((this.state.defaultPension + this.state.extraPension) * 1.2426) - this.state.otherMonthlyCosts) / 1.3142)
+    this.setState({
+      salary
+    })
+  }
+
+  getTotalPension = () => this.state.defaultPension + this.state.extraPension
+
+  componentDidMount() {
+    this.setSalary()
   }
 
   handleInputChange = e => {
     this.setState({
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: parseInt(e.target.value)
+    }, this.setSalary)
   }
 
   render() {
@@ -31,16 +48,17 @@ class Kalkylen extends React.Component {
               <input name="hourlyRate" type="number" value={this.state.hourlyRate} className={styles.input} onChange={this.handleInputChange} />
             </div>
             <div>
-              <label className={styles.label}>Procent av heltid</label>
-              <input name="employmentPercentage" type="number" value={this.state.employmentPercentage} className={styles.input} onChange={this.handleInputChange} />
+              <label className={styles.label}>Sjukdagar / Övrig frånvaro</label>
+              <input name="sickDays" type="number" value={this.state.sickDays} className={classNames(styles.input)} onChange={this.handleInputChange} />
             </div>
             <div>
-              <label className={styles.label}>Sjukdagar / Övrig frånvaro</label>
-              <input name="sickDays" type="number" value={this.state.sickDays} className={classNames(styles.input, styles.inputSmall)} onChange={this.handleInputChange} />
+              <label className={styles.label}>Extra semesterdagar</label>
+              <input name="extraVacation" type="number" value={this.state.extraVacation} className={classNames(styles.input)} onChange={this.handleInputChange} />
+              <label className={classNames(styles.label, styles.labelSmall)}>Utöver 30 dagars semester</label>
             </div>
             <div>
               <label className={styles.label}>Övriga månadskostnader</label>
-              <input name="monthlyCosts" type="number" value={this.state.monthlyCosts} className={styles.input} onChange={this.handleInputChange} />
+              <input name="otherMonthlyCosts" type="number" value={this.state.otherMonthlyCosts} className={styles.input} onChange={this.handleInputChange} />
               <label className={classNames(styles.label, styles.labelSmall)}>Telefon, tjänstebil, mm</label>
             </div>
             <div>
@@ -53,15 +71,15 @@ class Kalkylen extends React.Component {
         <div className={styles.results}>
           <div className={classNames(styles.result, styles.first)}>
             <span className={styles.resultLabel}>2019 antal dagar</span>
-            <span className={styles.resultValue}>253</span>
+            <span className={styles.resultValue}>{this.state.numberOfDays}</span>
           </div>
           <div className={classNames(styles.result)}>
             <span className={styles.resultLabel}>Total månadspension</span>
-            <span className={styles.resultValue}>2600</span>
+            <span className={styles.resultValue}>{this.getTotalPension()} kr</span>
           </div>
           <div className={classNames(styles.result, styles.highlight)}>
             <span className={styles.resultLabel}>Månadslön</span>
-            <span className={styles.resultValue}>5 miljoner!</span>
+            <span className={styles.resultValue}><b>{this.state.salary} kr</b></span>
           </div>
         </div>
       </div>
