9d58b8c46a9658dc3aa3916f2a9c0fec15aad276
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-12 12:24:47 +0200

    input handlers

--- src/Kalkylen.js
+++ src/Kalkylen.js
@@ -1,50 +1,72 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './Kalkylen.module.css';
-export default () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.inputContainer}>
-        <h2 className={styles.title}>Räkna ut din månadslön</h2>
-        <div className={styles.inputs}>
-          <div>
-            <label className={styles.label}>Timpris</label>
-            <input type="number" defaultValue="900" className={styles.input} />
+class Kalkylen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hourlyRate: 900,
+      employmentPercentage: 100,
+      sickDays: 10,
+      monthlyCosts: 1000,
+      extraPension: 0,
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className={styles.container} >
+        <div className={styles.inputContainer}>
+          <h2 className={styles.title}>Räkna ut din månadslön</h2>
+          <div className={styles.inputs}>
+            <div>
+              <label className={styles.label}>Timpris</label>
+              <input name="hourlyRate" type="number" value={this.state.hourlyRate} className={styles.input} onChange={this.handleInputChange} />
+            </div>
+            <div>
+              <label className={styles.label}>Procent av heltid</label>
+              <input name="employmentPercentage" type="number" value={this.state.employmentPercentage} className={styles.input} onChange={this.handleInputChange} />
+            </div>
+            <div>
+              <label className={styles.label}>Sjukdagar / Övrig frånvaro</label>
+              <input name="sickDays" type="number" value={this.state.sickDays} className={classNames(styles.input, styles.inputSmall)} onChange={this.handleInputChange} />
+            </div>
+            <div>
+              <label className={styles.label}>Övriga månadskostnader</label>
+              <input name="monthlyCosts" type="number" value={this.state.monthlyCosts} className={styles.input} onChange={this.handleInputChange} />
+              <label className={classNames(styles.label, styles.labelSmall)}>Telefon, tjänstebil, mm</label>
+            </div>
+            <div>
+              <label className={styles.label}>Extra pensionsavsättning</label>
+              <input name="extraPension" type="number" value={this.state.extraPension} className={styles.input} onChange={this.handleInputChange} />
+              <label className={classNames(styles.label, styles.labelSmall)}>Utöver 2600kr. per månad</label>
+            </div>
           </div>
-          <div>
-            <label className={styles.label}>Procent av heltid</label>
-            <input type="number" defaultValue="100" className={styles.input} />
+        </div>
+        <div className={styles.results}>
+          <div className={classNames(styles.result, styles.first)}>
+            <span className={styles.resultLabel}>2019 antal dagar</span>
+            <span className={styles.resultValue}>253</span>
           </div>
-          <div>
-            <label className={styles.label}>Sjukdagar / Övrig frånvaro</label>
-            <input type="number" defaultValue="10" className={classNames(styles.input, styles.inputSmall)} />
+          <div className={classNames(styles.result)}>
+            <span className={styles.resultLabel}>Total månadspension</span>
+            <span className={styles.resultValue}>2600</span>
           </div>
-          <div>
-            <label className={styles.label}>Övriga månadskostnader</label>
-            <input type="number" defaultValue="1000" className={styles.input} />
-            <label className={classNames(styles.label, styles.labelSmall)}>Telefon, tjänstebil, mm</label>
-          </div>
-          <div>
-            <label className={styles.label}>Extra pensionsavsättning</label>
-            <input type="number" defaultValue="0" className={styles.input} />
-            <label className={classNames(styles.label, styles.labelSmall)}>Utöver 2600kr. per månad</label>
+          <div className={classNames(styles.result, styles.highlight)}>
+            <span className={styles.resultLabel}>Månadslön</span>
+            <span className={styles.resultValue}>5 miljoner!</span>
           </div>
         </div>
       </div>
-      <div className={styles.results}>
-        <div className={classNames(styles.result, styles.first)}>
-          <span className={styles.resultLabel}>2019 antal dagar</span>
-          <span className={styles.resultValue}>253</span>
-        </div>
-        <div className={classNames(styles.result)}>
-          <span className={styles.resultLabel}>Total månadspension</span>
-          <span className={styles.resultValue}>2600</span>
-        </div>
-        <div className={classNames(styles.result, styles.highlight)}>
-          <span className={styles.resultLabel}>Månadslön</span>
-          <span className={styles.resultValue}>5 miljoner!</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
+
+export default Kalkylen
