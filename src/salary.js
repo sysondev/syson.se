3e89614f457c2028cd38bd766c699039f9f69bea
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-17 11:34:17 +0200

    min-salary 26k

--- src/salary.js
+++ src/salary.js
@@ -6,13 +6,22 @@ const DEFAULT_VALUES = {
     EXTRA_VACATION_DAYS: 0,
     OTHER_MONTHLY_COSTS: 1000,
     EXTRA_PENSION: 0,
+    BASE_SALARY: 26000,
 }
 
 const calculateSalary = ({ hourlyRate, sickDays = 0, extraVacationDays = 0,
-    extraPension = 0, otherMonthlyCosts = 0 }) =>
-    Math.round(((((DEFAULT_VALUES.NUMBER_OF_DAYS / 12) - ((sickDays + extraVacationDays) / 12) - 2.5)
+    extraPension = 0, otherMonthlyCosts = 0 }) => {
+    const salary = Math.round(((((DEFAULT_VALUES.NUMBER_OF_DAYS / 12) - ((sickDays + extraVacationDays) / 12) - 2.5)
         * (hourlyRate * 8 * 0.7))
         - ((DEFAULT_VALUES.DEFAULT_PENSION + extraPension) * 1.2426) - otherMonthlyCosts) / 1.3142)
+
+    if (salary < DEFAULT_VALUES.BASE_SALARY) {
+        return DEFAULT_VALUES.BASE_SALARY
+    }
+
+    return salary
+}
+
 
 
 export { calculateSalary, DEFAULT_VALUES }
