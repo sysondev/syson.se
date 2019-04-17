e1a71ef3ec094f9cb05acd3fd897a3ce18bc0aca
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-17 11:45:42 +0200

    mobile header padding

--- src/ArticleHeader.js
+++ src/ArticleHeader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo-green.png';
+import styles from './ArticleHeader.module.css';
+
 
 export default () => (
-  <header style={{ padding: '32px' }}>
+  <header className={styles.header}>
     <Link to='/'>
       <img
         src={logo}
         alt="Syson Logo"
-        style={{ width: '150px'}}
+        style={{ width: '150px' }}
       />
     </Link>
   </header>
