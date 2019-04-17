c15161334277385c308ff736bf92c1e78a485131
Soroush Hakami <soroush.hakami@gmail.com>
2019-04-17 11:37:32 +0200

    less padding

--- src/KalkylenArticle.js
+++ src/KalkylenArticle.js
@@ -3,7 +3,7 @@ import Kalkylen from './Kalkylen';
 
 export default () => {
   return (
-    <div className="container">
+    <div>
       <h1>Lek med lönemodellen</h1>
       <p>Med dina 70% gör du vad du vill. Det är inte mer än rimligt.</p>
       <p>Extra semesterdagar? Mer i pension? Hitta ett upplägg som passar dig.</p>
