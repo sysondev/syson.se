397a880576dbbfbf3f4a674493916619d08363d9
Soroush Hakami <soroush.hakami@gmail.com>
2019-05-04 10:21:41 +0200

    add JavaScript as a buzzword, remove docker

--- src/Buzzwords.js
+++ src/Buzzwords.js
@@ -13,12 +13,12 @@ export default () => {
       <div className={classNames('container', styles.content)}>
         <h1 className={styles.title}>Tekniker vi gillar</h1>
         <div className={classNames(styles.words)}>
+          <h3 className={classNames(styles.word)}>JavaScript</h3>
           <h3 className={classNames(styles.word)}>React</h3>
-          <h3 className={classNames(styles.word)}>Node</h3>
           <h3 className={classNames(styles.word)}>Java</h3>
           <h3 className={classNames(styles.word)}>.NET</h3>
           <h3 className={classNames(styles.word)}>Docker</h3>
-          <h3 className={classNames(styles.word)}>AWS</h3>
+          <h3 className={classNames(styles.word)}>Node</h3>
         </div>
       </div>
     </div>
