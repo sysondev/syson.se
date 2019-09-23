e5d2db9d435260edfd7ea053ec94f7063a631f59
Leo Danielsson <leo.danielsson@comhem.com>
2019-09-23 22:10:34 +0200

    make lazy load a bit less lazy

--- src/useLazy.js
+++ src/useLazy.js
@@ -1,16 +1,20 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import 'intersection-observer'; // Polyfill for IOS Safari
 
 export default (ref, load) => {
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if(entry.isIntersecting) {
-          load()
-          observer.disconnect();
-        }
-      });
-    })
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            console.log('intersected!');
+            load();
+            observer.disconnect();
+          }
+        });
+      },
+      { rootMargin: '100%' }
+    );
     observer.observe(ref.current);
   }, []);
-} 
+};
