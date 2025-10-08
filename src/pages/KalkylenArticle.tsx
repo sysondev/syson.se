d641eccb735b7938462c67392b70ecdcc34fe5ac
Love Gehlin <love.gehlin@instabox.se>
2025-10-08 16:05:11 +0200

    fix copy

--- src/pages/KalkylenArticle.tsx
+++ src/pages/KalkylenArticle.tsx
@@ -5,7 +5,7 @@ export default function KalkylenArticle() {
   return (
     <div>
       <h1>Lönemodellen i detalj</h1>
-      <p>Med dina 70% gör du vad du vill. Det är inte mer än rimligt.</p>
+      <p>Med dina 75% gör du vad du vill. Det är inte mer än rimligt.</p>
       <p>
         Extra semesterdagar? Mer i pension? Hitta ett upplägg som passar dig.
       </p>
